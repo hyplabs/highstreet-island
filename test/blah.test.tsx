a3d64538bf4ce3247aed972bc3fd97b09c89eb66
Larry <larryoatmeal@gmail.com>
2021-06-18 13:05:52 -0700

    Fix test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { HighstreetIsland } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(
+      <HighstreetIsland style={{}} worldConfig={{ gltfPath: '' }} />,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
