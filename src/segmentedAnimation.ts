a768e2e52f1ce563a41bb1e521a33608d5070129
larryoatmeal <larryoatmeal@gmail.com>
2021-06-24 17:26:20 -0700

    Add missing file

---
+++ src/segmentedAnimation.ts
@@ -0,0 +1,32 @@
+export interface PathSegment {
+  startTime: number;
+  update: (progress: number, isStart: boolean) => void;
+}
+
+export function segmentedAnimation(segments: PathSegment[]) {
+  let lastSegment: PathSegment | null = null;
+  function getActiveSegment(tCycle: number) {
+    for (let i = 0; i < segments.length; i++) {
+      if (tCycle < segments[i].startTime) {
+        return i - 1;
+      }
+    }
+    throw new Error('SHOULD NEVER REACH HERE');
+  }
+  function animateRocket(t: number) {
+    const tCycle = t % segments[segments.length - 1].startTime;
+    const activeSegmentIndex = getActiveSegment(tCycle);
+    const activeSegment = segments[activeSegmentIndex];
+    const endTime = segments[activeSegmentIndex + 1].startTime;
+    const cycleProgress =
+      (tCycle - activeSegment.startTime) / (endTime - activeSegment.startTime);
+
+    activeSegment.update(cycleProgress, lastSegment !== activeSegment);
+    if (lastSegment !== activeSegment) {
+      lastSegment = activeSegment;
+    }
+    // throttleLog(cycleProgress);
+  }
+
+  return animateRocket;
+}
